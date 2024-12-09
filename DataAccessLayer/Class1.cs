58d9f2499e6189b9ff0539faad225d4bffd4f6ad
EverardoTV <eveviera244@gmail.com>
2024-12-09 12:52:55 -0600

    Cadena de conexion

--- DataAccessLayer/Class1.cs
+++ DataAccessLayer/Class1.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccessLayer
 {
-    public class Class1
+    public class configuracion
     {
+        //Cadena de conexión
+        //Data Source = nombre del servidor de BD
+        //LocalHost
+        //.
+        //Nombre de mi instancia
+        //Initial Catalog = nombre de la BD
+        //Integrated Security = true (Credenciales de la máquina)
+        //= false (Credeniales de acceso)
+        //Se habilitan los campos de
+        //User =;
+        //Password =;
+
+        static string _cadenaConexion = @"Data Source = DESKTOP-3KBJO3H\SQLEXPRESS;
+                                           Initial Catalog = Transportes;
+                                           Integrated Security = true;";
+
+        //Encapsulamiento
+
+        public static string CadenaConexion
+        {
+            get
+            {
+                return _cadenaConexion;
+            }
+        }
     }
 }
