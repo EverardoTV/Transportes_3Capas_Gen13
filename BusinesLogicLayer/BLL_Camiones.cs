e74dbc4c91fb88fcc1e7e3a095aa2dc26210b02b
EverardoTV <eveviera244@gmail.com>
2024-12-09 14:54:23 -0600

    BLL-Camiones(Read)

---
+++ BusinesLogicLayer/BLL_Camiones.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+using ViewObjects;
+
+namespace BusinesLogicLayer
+{
+    public class BLL_Camiones
+    {
+        //Create
+
+
+
+        //Read
+        public static List<Camiones_VO> Get_Camiones(params object[] parametros)
+        {
+            return DAL_Camiones.Get_Camiones(parametros);
+        }
+
+
+        //Update
+
+
+
+        //Delete
+    }
+}
